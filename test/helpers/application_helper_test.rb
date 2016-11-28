62b76a0cf59d69f07c7434bee9e203131b698375
Bryan <bryandhunt@gmail.com>
2016-11-27 22:24:49 -0500

    updating app helper test to green

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,9 +2,10 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,         FILL_IN
-    assert_equal full_title("Help"), FILL_IN
-    assert_equal full_title("About"), FILL_IN
-    assert_equal full_title("Contact"), FILL_IN
+    assert_equal full_title,            "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"),    "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("About"),   "About | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Contact"), "Contact | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Sign up"), "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
