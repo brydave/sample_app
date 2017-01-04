10d52442041e3e35582719d3994c0805cb63a72f
Bryan <bryandhunt@gmail.com>
2017-01-04 10:09:50 -0500

    Fixing an issue in the create user integration test Adding sessions helper and functionality

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,7 +7,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: { name: "",
+      post users_path, params: { user: { name: "",
                                         email: "user@invalid",
                                         password: "foo",
                                         password_confirmation: "bar" } }
@@ -21,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count' do
-      post signup_path, params: { user: { name: "Example User",
+      post users_path, params: { user: { name: "Example User",
                                          email: "example@example.com",
                                          password: "password",
                                          password_confirmation: "password"} }
