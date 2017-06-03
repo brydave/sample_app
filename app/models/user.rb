6e26d05ecdad08d59ea74cc7bdcd48a835aef648
Bryan <bryandhunt@gmail.com>
2017-06-03 09:02:11 -0400

    Finished 13.3.3 proto-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,6 +66,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  def feed
+    microposts
+  end
+
   private
     # Converts email to all lower-case.
     def downcase_email
